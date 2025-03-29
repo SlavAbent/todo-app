8033bb1ba8822b968481bf9766f3f625365b067e
@abentss <abent@softlab.ru>
2025-03-29 17:08:04 +0500

    add tests

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
-import path from "path";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tsconfigPaths()],
 });
